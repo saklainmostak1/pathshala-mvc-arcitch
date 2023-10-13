7cb3601f64d491b55032f58e93ab1a00584c9027
saklainmostak1 <saklain15-12003@diu.edu.bd>
2023-10-13 20:33:06 +0600

    push

--- src/app/(view)/admin/user_roles/user_role_creates/page.js
+++ src/app/(view)/admin/user_roles/user_role_creates/page.js
@@ -3,8 +3,7 @@ import React from 'react';
 import '../../../admin_layout/modal/fa.css'
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
-import { useEffect } from 'react';
-import { event } from 'jquery';
+
 const UsersRoleCreates = () => {
 
 
